2881d9cc6b4e9017db6986089f5f3db9a253936f
Martin Gutierrez <gutix007@hotmail.com>
2013-10-21 20:23:47 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flipper::Application.config.secret_key_base = '0830a8a4e578f8014976ad0e9ad63e2ff947d9312f4ec90682e2bcbfc1c23fafa8cc3d96eff1a2f892082872ac07c2acfde28b0a7457b342a8c738677627e905'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flipper::Application.config.session_store :cookie_store, key: '_flipper_session'
